e555bca4c7a4a251643eeb1e29a0397bd4e658cc
Diana Vintila <dianavintila25@gmail.com>
2020-11-23 00:00:36 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+
+    public static void main(String args[]) {
+
+        for(int i=1;i<=100;i++) {
+
+                if (i % 7 == 0)
+                    System.out.print(" Rizz, ");
+                else {
+                    if (i % 11 == 0)
+                        System.out.print(" Jazz, ");
+                    else
+                        System.out.print(" " + i + ", ");
+
+
+
+                }
+        }
+
+
+
+
+    }
+
+
+}
+
